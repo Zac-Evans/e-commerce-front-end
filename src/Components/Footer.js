aaba2eded21541e71907f3bf7d4387da57faac0f
zac-evans <zacevanscoding@gmail.com>
2020-11-05 18:34:29 -0500

    fixed cart count

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,21 +1,8 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import Logo from "../images/logo-light.png";
-import LogOut from "./Admin_Components/LogOutBtn";
-import Login from "./Admin_Components/LoginBtn";
-import AdminBtn from "./Admin_Components/AdminBtn";
-import CartIcon from "../images/cart-icon.png";
-import CartCounter from "./CartCounter";
-import History from "./Admin_Components/History";
 
 class Footer extends Component {
   render() {
