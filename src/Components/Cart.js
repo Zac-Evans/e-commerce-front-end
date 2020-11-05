d012e19263432d025840d8d9a26c3d1d7a344330
zac-evans <zacevanscoding@gmail.com>
2020-11-05 13:58:33 -0500

    delete cart route

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -15,49 +15,54 @@ class Cart extends Component {
 
   componentDidMount() {
     if (!sessionStorage.getItem("userId")) {
-      let productArray = JSON.parse(sessionStorage.getItem("cart"));
-      axios
-        .post(
-          "https://e-commerce-project-2020.herokuapp.com/show-cart/logged-out",
-          {
-            products: productArray,
-          }
-        )
-        .then((result) => {
-          function countProducts(arr) {
-            var a = [],
-              b = [],
-              prev;
+      let thisCart = JSON.parse(sessionStorage.getItem("cart"));
+      console.log(thisCart.length);
 
-            arr.sort();
-            for (var i = 0; i < arr.length; i++) {
-              if (arr[i] !== prev) {
-                a.push(arr[i]);
-                b.push(1);
-              } else {
-                b[b.length - 1]++;
-              }
-              prev = arr[i];
+      if (thisCart?.length) {
+        let productArray = JSON.parse(sessionStorage.getItem("cart"));
+        axios
+          .post(
+            "https://e-commerce-project-2020.herokuapp.com/show-cart/logged-out",
+            {
+              products: productArray,
             }
+          )
+          .then((result) => {
+            function countProducts(arr) {
+              var a = [],
+                b = [],
+                prev;
 
-            return [a, b];
-          }
-          var productCounts = countProducts(productArray);
-          var productCounter = productCounts[1];
+              arr.sort();
+              for (var i = 0; i < arr.length; i++) {
+                if (arr[i] !== prev) {
+                  a.push(arr[i]);
+                  b.push(1);
+                } else {
+                  b[b.length - 1]++;
+                }
+                prev = arr[i];
+              }
 
-          let allProducts = result.data.map((item, index) => {
-            let product = Object.assign({}, item);
-            product.quantity = productCounter[index];
-            return product;
+              return [a, b];
+            }
+            var productCounts = countProducts(productArray);
+            var productCounter = productCounts[1];
+
+            let allProducts = result.data.map((item, index) => {
+              let product = Object.assign({}, item);
+              product.quantity = productCounter[index];
+              return product;
+            });
+
+            this.setState({
+              products: allProducts,
+            });
+          })
+          .catch((error) => {
+            console.log("error");
           });
-
-          this.setState({
-            products: allProducts,
-          });
-        })
-        .catch((error) => {
-          console.log("error");
-        });
+      }
     } else {
       axios
         .get(
@@ -77,23 +82,22 @@ class Cart extends Component {
     return (
       <div style={{ maxWidth: "600px" }}>
         <h2 className="mt-5 mb-5 text-center">Your shopping cart:</h2>
-        {this.state.products
-          ? this.state.products.map((item, index) => (
-              <CartItem
-                key={item.id}
-                id={item.id}
-                product_name={item.product_name}
-                shortDescription={item.shortDescription}
-                longDescription={item.longDescription}
-                photo={item.photo}
-                price={item.price}
-                quantity={item.quantity}
-                rating={item.rating}
-                rating_count={item.rating_count}
-                // rerenderParentCallback={this.rerenderParentCallback}
-              />
-            ))
-          : "Your cart is empty."}
+        {this.state.products &&
+          this.state.products.map((item, index) => (
+            <CartItem
+              key={item.id}
+              id={item.id}
+              product_name={item.product_name}
+              shortDescription={item.shortDescription}
+              longDescription={item.longDescription}
+              photo={item.photo}
+              price={item.price}
+              quantity={item.quantity}
+              rating={item.rating}
+              rating_count={item.rating_count}
+              // rerenderParentCallback={this.rerenderParentCallback}
+            />
+          ))}
       </div>
     );
   }
