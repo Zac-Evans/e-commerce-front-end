4d787dd0ddb7461e8a1633bd647a78e2872c61b2
zac-evans <zacevanscoding@gmail.com>
2021-02-11 12:22:24 -0500

    added loading spinner on ShopContainer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import Detailedhistory from "./Components/Detailedhistory";
-import Header from "./Components/Header";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Route, Switch } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Register from "./Components/Register";
-import Login from "./Components/Login";
-import Admin from "./Components/Admin_Components/Admin";
-import NewProduct from "./Components/Admin_Components/NewProduct";
-import UpdateProduct from "./Components/Admin_Components/UpdateProduct";
-import Delete from "./Components/Admin_Components/Delete";
-import ShopContainer from "./Components/ShopContainer";
-import Checkout from "./Components/Checkout";
-import CompleteOrderhistory from "./Components/CompleteOrderHistory";
-import Footer from "./Components/Footer";
+import React, { Component } from 'react';
+import Detailedhistory from './Components/Detailedhistory';
+import Header from './Components/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from './Components/Register';
+import Login from './Components/Login';
+import Admin from './Components/Admin_Components/Admin';
+import NewProduct from './Components/Admin_Components/NewProduct';
+import UpdateProduct from './Components/Admin_Components/UpdateProduct';
+import Delete from './Components/Admin_Components/Delete';
+import ShopContainer from './Components/ShopContainer';
+import Checkout from './Components/Checkout';
+import CompleteOrderhistory from './Components/CompleteOrderHistory';
+import Footer from './Components/Footer';
 
 export default class App extends Component {
   render() {
@@ -21,25 +21,23 @@ export default class App extends Component {
       <div>
         <Router>
           <Switch>
-            <div className="d-flex flex-column min-vh-100">
+            <div className='d-flex flex-column min-vh-100'>
               <Header />
-              <Route exact path="/" component={ShopContainer} />
-
-              <Route path="/register" component={Register} />
-              <Route path="/login" component={Login} />
-              <Route exact path="/admin" component={Admin} />
-              <Route path="/admin/create" component={NewProduct} />
-              <Route path="/admin/update" component={UpdateProduct} />
-              <Route path="/admin/delete" component={Delete} />
-              <Route path="/shop" component={ShopContainer} />
-              <Route path="/checkout" component={Checkout} />
-
+              <Route exact path='/' component={ShopContainer} />
+              <Route path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/admin' component={Admin} />
+              <Route path='/admin/create' component={NewProduct} />
+              <Route path='/admin/update' component={UpdateProduct} />
+              <Route path='/admin/delete' component={Delete} />
+              <Route path='/shop' component={ShopContainer} />
+              <Route path='/checkout' component={Checkout} />
               <Route
-                path="/order-history/user"
+                path='/order-history/user'
                 component={CompleteOrderhistory}
               />
               <Route
-                path="/order-history/order="
+                path='/order-history/order='
                 component={CompleteOrderhistory}
               />
               <Footer />
--- src/Components/ShopContainer.js
+++ src/Components/ShopContainer.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-import axios from "axios";
-import ProductCard from "./ProductCard";
-import Product from "./Product";
-import Image from "react-bootstrap/Image";
-import Background from "../images/fruit-background.jpeg";
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import axios from 'axios';
+import ProductCard from './ProductCard';
+import Product from './Product';
+import Image from 'react-bootstrap/Image';
+import Spinner from 'react-bootstrap/Spinner';
+import Background from '../images/fruit-background.jpeg';
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class ShopContainer extends Component {
   constructor(state) {
     super(state);
 
     this.state = {
-      inputValue: "",
+      inputValue: '',
       products: [],
       cart_count: 0,
       // update: 0,
@@ -31,18 +32,18 @@ class ShopContainer extends Component {
   // };
 
   filterOnChange = (event) => {
-    console.log("hi from onChange", event.target.value);
+    console.log('hi from onChange', event.target.value);
     this.setState({
       inputValue: event.target.value,
     });
   };
 
   componentDidMount() {
-    if (!sessionStorage.getItem("userId")) {
-      if (sessionStorage.getItem("cart")) {
+    if (!sessionStorage.getItem('userId')) {
+      if (sessionStorage.getItem('cart')) {
         this.setState({
           cart_count: JSON.parse(
-            sessionStorage.getItem("cart").split(",").length
+            sessionStorage.getItem('cart').split(',').length
           ),
         });
       }
@@ -50,7 +51,7 @@ class ShopContainer extends Component {
       axios
         .get(
           `https://e-commerce-project-2020.herokuapp.com/count-cart/${sessionStorage.getItem(
-            "userId"
+            'userId'
           )}`
         )
         .then((res) => {
@@ -58,12 +59,12 @@ class ShopContainer extends Component {
         });
     }
     return axios
-      .get("https://e-commerce-project-2020.herokuapp.com/inventory/")
+      .get('https://e-commerce-project-2020.herokuapp.com/inventory/')
       .then((res) => {
         this.setState({ products: res.data });
       })
       .catch((error) => {
-        console.log("error");
+        console.log('error');
       });
   }
   render() {
@@ -74,18 +75,18 @@ class ShopContainer extends Component {
     // });
     return (
       <div>
-        {this.state.products && (
-          <Container className="pl-0 pr-0">
+        {this.state.products ? (
+          <Container className='pl-0 pr-0'>
             <Router>
               <Switch>
-                <Route exact path="/">
+                <Route exact path='/'>
                   <Image
                     fluid
-                    width="1200px"
-                    className="m-0"
+                    width='1200px'
+                    className='m-0'
                     src={Background}
                   />
-                  <Row className="d-flex justify-content-center">
+                  <Row className='d-flex justify-content-center'>
                     {this.state.products.map((item, index) => (
                       <ProductCard
                         key={item.id}
@@ -124,6 +125,12 @@ class ShopContainer extends Component {
               </Switch>
             </Router>
           </Container>
+        ) : (
+          <div>
+            <Spinner animation='border' role='status'>
+              <span className='sr-only'>Loading...</span>
+            </Spinner>
+          </div>
         )}
       </div>
     );
